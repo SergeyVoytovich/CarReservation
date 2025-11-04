1b7aba1582e18e996630f57855bb720f69da1cc7
Sergey Voytovich <s.voytovich@outlook.com>
2025-11-04 12:19:16 +0000

    iplement base repository

---
+++ src/CarReservation/CarReservation.Web/Data/RepositoryBase.cs
@@ -0,0 +1,9 @@
+﻿
+namespace CarReservation.Web.Data;
+
+public abstract class RepositoryBase<T>(IEnumerable<T> items) : IRepository<T>
+{
+    protected virtual IEnumerable<T> Items { get; } = items;
+
+    public Task<IList<T>> GetAsync() => Task.FromResult<IList<T>>(Items.ToList());
+}
